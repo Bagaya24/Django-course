609291cc3c61c7c75780a9fc480c88d5300b9961
bagaya <bagayafazili@gmail.com>
2026-08-11 14:18:27 +0200

    feat: -added allowed hosts for railway

--- ecommerce-django/ecom/settings.py
+++ ecommerce-django/ecom/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['django-course-production-a7b4.up.railway.app', 'https://django-course-production-a7b4.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['django-course-production-a7b4.up.railway.app', 'https://django-course-production-a7b4.up.railway.app']
 
 # Application definition
 
